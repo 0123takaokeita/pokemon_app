810ed1b9ea546c86e7363c32144fc08280675ad4
Takao <k.takao@neogenia.co.jp>
2022-09-23 16:50:26 +0900

    feat: pokemonAPI 全件取得を確認

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
+import {useEffect, useState} from 'react';
 import './App.css';
+import { getAllPokemon, getPokemon } from './utils/pokemon';
+import Card from './components/Card/Card';
 
 function App() {
+  const initialURL = 'https://pokeapi.co/api/v2/pokemon';
+  const [loading, setLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState([]);
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      // 全てもポケモンを取得
+      let res = await getAllPokemon(initialURL);
+      // 詳細取得
+      loadPokemon(res.results);
+      setNextURL(res.next);
+      setPrevURL(res.previous);
+      setLoading(false);
+    }
+    fetchPokemonData();
+  }, [])
+
+  const loadPokemon = async (data) => {
+    let _pokemoData = await Promise.all(
+      data.map((pokemon) => {
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+    setPokemonData(_pokemoData);
+  };
+
   return (
+      <div className="App">
+        {loading ? (
+          <h1>ロード中・・・</h1>
+        ) : (
+          <div className='pokemonCardContainer'>
+            {pokemonData.map((pokemon, i) => {
+              return <Card key={i} pokemon={pokemon}/>
+            })}
+          </div>
+        ) }
+      </div>
   );
 }
 
